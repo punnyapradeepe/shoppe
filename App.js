ffae14889e57a6edc14331758aa92329163e882d
punnyapradeepe <punnyapradeep1328@gmail.com>
2024-07-09 13:50:05 +0530

    hhh

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import LoginScreen from './App/Screens/LoginScreen';
 import { useFonts } from 'expo-font';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import CreateAcc from './App/Screens/CreateAcc';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 let customFonts = {
   'Raleway': require('./assets/Fonts/Raleway-Regular.ttf'),
-  'RalewayM':require('./assets/Fonts/Raleway-Medium.ttf'),
-  'RalewayEL':require('./assets/Fonts/Raleway-ExtraBold.ttf'),
-  'RalewayB':require('./assets/Fonts/Raleway-Bold.ttf')
+  'RalewayM': require('./assets/Fonts/Raleway-Medium.ttf'),
+  'RalewayEL': require('./assets/Fonts/Raleway-ExtraBold.ttf'),
+  'RalewayB': require('./assets/Fonts/Raleway-Bold.ttf'),
 };
+
 export default function App() {
   const [isLoaded] = useFonts(customFonts);
 
+  if (!isLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
-    <>
-    {isLoaded?<View style={styles.container}>
-      <LoginScreen/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginScreen">
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }}/>
+        <Stack.Screen name="createAcc" component={CreateAcc} options={{ headerShown: false }}/>
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>:
-    <ActivityIndicator />}
-    </>
+    </NavigationContainer>
   );
 }
 
--- App/Screens/LoginScreen.js
+++ App/Screens/LoginScreen.js
@@ -1,34 +1,35 @@
-import { View, Image, StyleSheet, Text, ActivityIndicator, TouchableOpacity } from 'react-native'
-import React from 'react'
-import Colors from '../Utils/Colors'
-
-
+import { View, Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import React from 'react';
+import Colors from '../Utils/Colors';
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 export default function LoginScreen() {
+  const navigation = useNavigation();
+
   return (
-   
     <View style={styles.container}>
       <View style={styles.ellipse}>
         <Image source={require('./../../assets/Images/loginImg.png')} style={styles.loginImg} />
-      </View >
+      </View>
       <Text style={styles.text}>Shoppe</Text>
-      <View style={{alignItems:'center',padding:10
-      }}>
-      <Text>BeautifulneCommerce UI Kit</Text>
-      <Text style={{padding:10}}>for your online store</Text>
+      <View style={{ alignItems: 'center', padding: 10 }}>
+        <Text>Beautiful eCommerce UI Kit</Text>
+        <Text style={{ padding: 10 }}>for your online store</Text>
       </View>
       <View>
-      
-      <TouchableOpacity  style={styles.button}> 
-          <Text style={styles.buttonText}> Let's Get Started </Text>
-      </TouchableOpacity>
-     <View>
-      <Text>I already have an a</Text>
-     </View>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('createAcc')}>
+          <Text style={styles.buttonText}>Let's Get Started</Text>
+        </TouchableOpacity>
+        <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', paddingLeft: 50 }}>
+          <Text style={{ padding: 10 }}>I already have an account</Text>
+          <TouchableOpacity style={styles.circleButton}>
+            <AntDesign name="arrowright" size={24} color={Colors.WHITE} />
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
-  
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -41,24 +42,24 @@ const styles = StyleSheet.create({
     width: 134,
     height: 134,
     borderRadius: 67,
-    backgroundColor: Colors.WHITE,
+    backgroundColor: 'white',
     justifyContent: 'center',
     alignItems: 'center',
   },
   loginImg: {
-    width: 90, 
+    width: 90,
     height: 90,
-    resizeMode: 'contain', 
+    resizeMode: 'contain',
   },
-  text:{
-   fontFamily:'RalewayB',
-   fontSize:50,
-   paddingTop:10
+  text: {
+    fontFamily: 'RalewayB',
+    fontSize: 50,
+    paddingTop: 10,
   },
   button: {
-   height:61,
-   width:330,
-   marginTop:63,
+    height: 61,
+    width: 330,
+    marginTop: 63,
     backgroundColor: Colors.PRIMARY,
     justifyContent: 'center',
     alignItems: 'center',
@@ -69,8 +70,15 @@ const styles = StyleSheet.create({
   buttonText: {
     color: Colors.WHITE,
     fontSize: 18,
-    fontFamily:'Regular'
+    fontFamily: 'Regular',
   },
-
-}
-)
+  circleButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    backgroundColor: Colors.PRIMARY,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+});
