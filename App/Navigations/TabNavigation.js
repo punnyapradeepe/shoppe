65697b5ee642f9fd09cc86e584522505156727f5
PUNNYA PRADEEP <punnyapradeep1328@gmail.com>
2024-08-07 15:30:02 +0530

    .

--- App/Navigations/TabNavigation.js
+++ App/Navigations/TabNavigation.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeScreen from './../Screens/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
+import { HomeImg, Heart, Document, Shopping, AccountImg } from '../Utils/SvgIcons';
+import ActivityScreen from '../Screens/ActivityScreen';
 import WishListScreen from './../Screens/WishListScreen';
 import CategoriesScreen from './../Screens/CategoriesScreen';
 import ShopScreen from './../Screens/ShopScreen';
 import ProfileScreen from './../Screens/ProfileScreen';
-import ActivityScreen from '../Screens/ActivityScreen';
-import { HomeImg, Heart, Document, Shopping, AccountImg } from '../Utils/SvgIcons';
-import { StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AllCategories from '../Screens/AllCategories';
-import FlashSale from '../../Components/FlashSale';
 import FlashSaleScreen from '../Screens/FlashSaleScreen';
 import ShippingAddressScreen from '../Screens/ShippingAddressScreen';
 import PaymentScreen from '../Screens/PaymentScreen';
-import PaymentMethod from './../Screens/PaymentMethod'
+import PaymentMethod from './../Screens/PaymentMethod';
 import Shipping from '../Screens/Shipping';
 import MyProfile from '../Screens/MyProfile';
 import MostPopularDetail from '../Screens/MostPopularDetail';
@@ -26,7 +23,7 @@ import NewItemDetail from '../Screens/NewItemDetail';
 import orderDetailScreen from '../Screens/orderDetailScreen';
 
 const Tab = createBottomTabNavigator();
-const Stack= createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const TabNavigation = () => {
   return (
@@ -38,7 +35,7 @@ const TabNavigation = () => {
         tabBarStyle: styles.tabBar,
       }}
     >
-      <Tab.Screen
+         <Tab.Screen
         name='Activity'
         component={ActivityStack}
         options={{
@@ -50,6 +47,16 @@ const TabNavigation = () => {
       <Tab.Screen
         name='Wishlist'
         component={WishListScreen}
+        listeners={({ navigation }) => ({
+          tabPress: e => {
+            e.preventDefault(); // Prevent default behavior
+            navigation.navigate('Wishlist');
+            navigation.reset({
+              index: 0,
+              routes: [{ name: 'Wishlist' }],
+            });
+          },
+        })}
         options={{
           tabBarIcon: ({ color, size, focused }) => (
             <Heart width={size} height={size} fill={focused ? 'black' : 'blue'} />
@@ -59,6 +66,16 @@ const TabNavigation = () => {
       <Tab.Screen
         name='Categoriesstack'
         component={CategoriesStack}
+        listeners={({ navigation }) => ({
+          tabPress: e => {
+            e.preventDefault(); // Prevent default behavior
+            navigation.navigate('Categoriesstack');
+            navigation.reset({
+              index: 0,
+              routes: [{ name: 'Categoriesstack' }],
+            });
+          },
+        })}
         options={{
           tabBarIcon: ({ color, size, focused }) => (
             <Document width={size} height={size} fill={focused ? 'black' : 'blue'} />
@@ -68,6 +85,16 @@ const TabNavigation = () => {
       <Tab.Screen
         name='Shop'
         component={ShopScreenStack}
+        listeners={({ navigation }) => ({
+          tabPress: e => {
+            e.preventDefault(); // Prevent default behavior
+            navigation.navigate('Shop');
+            navigation.reset({
+              index: 0,
+              routes: [{ name: 'Shop' }],
+            });
+          },
+        })}
         options={{
           tabBarIcon: ({ color, size, focused }) => (
             <Shopping width={size} height={size} fill={focused ? 'black' : 'blue'} />
@@ -77,65 +104,68 @@ const TabNavigation = () => {
       <Tab.Screen
         name='Profile'
         component={ProfileScreenStack}
+        listeners={({ navigation }) => ({
+          tabPress: e => {
+            e.preventDefault(); 
+            navigation.navigate('Profile');
+            navigation.reset({
+              index: 0,
+              routes: [{ name: 'Profile' }],
+            });
+          },
+        })}
         options={{
           tabBarIcon: ({ color, size, focused }) => (
             <AccountImg width={size} height={size} fill={focused ? 'black' : 'blue'} />
           ),
         }}
       />
-      
-
     </Tab.Navigator>
   );
 };
 
+const ActivityStack = () => (
+  <Stack.Navigator initialRouteName="activity">
+    <Stack.Screen name="activity" component={ActivityScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="allc" component={AllCategories} options={{ headerShown: false }} />
+    <Stack.Screen name="flashSale" component={FlashSaleScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="populardetail" component={MostPopularDetail} options={{ headerShown: false }} />
+    <Stack.Screen name="justforyou" component={JustForYou} options={{ headerShown: false }} />
+    <Stack.Screen name="justforyoudetail" component={JustForYouDetail} options={{ headerShown: false }} />
+    <Stack.Screen name="clothing" component={clothingScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="NewItemDetail" component={NewItemDetail} options={{ headerShown: false }} />
+  </Stack.Navigator>
+);
 
-const ActivityStack=()=>(
+const ShopScreenStack = () => (
+  <Stack.Navigator initialRouteName="shop">
+    <Stack.Screen name="shop" component={ShopScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="shippingAddr" component={ShippingAddressScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="payment" component={PaymentScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="orderscreen" component={orderDetailScreen} options={{ headerShown: false }} />
+  </Stack.Navigator>
+);
 
-<Stack.Navigator initialRouteName="activity">
-<Stack.Screen name="activity" component={ActivityScreen} options={{ headerShown: false }}/>
-<Stack.Screen name="allc" component={AllCategories} options={{ headerShown:false}}/>
-<Stack.Screen name="flashSale" component={FlashSaleScreen} options={{ headerShown:false}}/>
-<Stack.Screen name="populardetail" component={MostPopularDetail} options={{ headerShown:false}}/>
-<Stack.Screen name="justforyou" component={JustForYou} options={{ headerShown:false}}/>
-<Stack.Screen name="justforyoudetail" component={JustForYouDetail} options={{ headerShown:false}}/>
-<Stack.Screen name="clothing" component={clothingScreen} options={{ headerShown:false}}/>
-<Stack.Screen name="NewItemDetail" component={NewItemDetail} options={{ headerShown:false}}/>
-</Stack.Navigator>
+const ProfileScreenStack = () => (
+  <Stack.Navigator initialRouteName="Profile">
+    <Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="paymentMethod" component={PaymentMethod} options={{ headerShown: false }} />
+    <Stack.Screen name="Shipping" component={Shipping} options={{ headerShown: false }} />
+    <Stack.Screen name="myprofile" component={MyProfile} options={{ headerShown: false }} />
+    <Stack.Screen name="orderscreen" component={orderDetailScreen} options={{ headerShown: false }} />
+  </Stack.Navigator>
+);
 
-)
-
-const ShopScreenStack=()=>(
-<Stack.Navigator initialRouteName="shop">
-<Stack.Screen name="shop" component={ShopScreen} options={{ headerShown: false }}/>
-<Stack.Screen name="shippingAddr" component={ShippingAddressScreen} options={{ headerShown:false}}/>
-<Stack.Screen name="payment" component={PaymentScreen} options={{ headerShown:false}}/>
-<Stack.Screen name="orderscreen" component={orderDetailScreen} options={{ headerShown:false}}/>
-
-</Stack.Navigator>
-)
-
-const ProfileScreenStack=()=>(
-<Stack.Navigator initialRouteName="Profile">
-<Stack.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false }}/>
-<Stack.Screen name="paymentMethod" component={PaymentMethod} options={{ headerShown:false}}/>
-<Stack.Screen name="Shipping" component={Shipping} options={{ headerShown:false}}/>
-<Stack.Screen name="myprofile" component={MyProfile} options={{ headerShown:false}}/>
-<Stack.Screen name="orderscreen" component={orderDetailScreen} options={{ headerShown:false}}/>
-
-</Stack.Navigator>
-)
-
-const CategoriesStack=()=>(
-<Stack.Navigator initialRouteName="Categoriesstack">
-<Stack.Screen name="allc" component={AllCategories} options={{ headerShown:false}}/>
-<Stack.Screen name="clothing" component={clothingScreen} options={{ headerShown:false}}/>
-<Stack.Screen name="justforyoudetail" component={JustForYouDetail} options={{ headerShown:false}}/>
-<Stack.Screen name="populardetail" component={MostPopularDetail} options={{ headerShown:false}}/>
-<Stack.Screen name="flashSale" component={FlashSaleScreen} options={{ headerShown:false}}/>
-<Stack.Screen name="NewItemDetail" component={NewItemDetail} options={{ headerShown:false}}/>
-</Stack.Navigator>
-)
+const CategoriesStack = () => (
+  <Stack.Navigator initialRouteName="Categoriesstack">
+    <Stack.Screen name="allc" component={AllCategories} options={{ headerShown: false }} />
+    <Stack.Screen name="clothing" component={clothingScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="justforyoudetail" component={JustForYouDetail} options={{ headerShown: false }} />
+    <Stack.Screen name="populardetail" component={MostPopularDetail} options={{ headerShown: false }} />
+    <Stack.Screen name="flashSale" component={FlashSaleScreen} options={{ headerShown: false }} />
+    <Stack.Screen name="NewItemDetail" component={NewItemDetail} options={{ headerShown: false }} />
+  </Stack.Navigator>
+);
 
 const styles = StyleSheet.create({
   tabBar: {
